e33e114c84d4188c27df7501186cc9875614a2d0
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-18 15:14:55 +0100

    add general structure

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // 2.poi chiedi il suo cognome,
 // 3.poi chiedi il suo colore preferito
 // 4.Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+// 1.chiedi all'utente il suo nome
+let Nome = prompt('Inserisci il tuo nome');
+console.log(Nome);
+// 2.chiedi all'utente il suo cognome
+let Cognome = prompt('Inserisci il tuo cognome');
+console.log(Cognome);
+// 3.chiedi all'utente il suo colore preferito
+// 4.genera la password sullo schermo unendo i valori + 21
